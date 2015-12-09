62af4f8d9acd6912317bcf00b60cbec2535a0a36
Saravana <saravanahacker@gmail.com>
2015-12-10 00:28:05 +0530

    main class modified

--- spring-boot/src/main/java/com/sahajsoft/trade/Application.java
+++ spring-boot/src/main/java/com/sahajsoft/trade/Application.java
@@ -1,21 +1,13 @@
 package com.sahajsoft.trade;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		String[] beans = context.getBeanDefinitionNames();
-		Arrays.sort(beans);
-		for (String bean : beans) {
-			System.out.println(bean);
-		}
+		SpringApplication.run(Application.class, args);
 	}
 
 }
