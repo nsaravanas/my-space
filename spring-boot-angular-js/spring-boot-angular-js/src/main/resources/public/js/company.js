9c28d498f5b4b23188ee0c6bbd50e4a842b006bf
Saravana <saravanahacker@gmail.com>
2016-02-14 00:39:42 +0530

    UI enhanced

--- spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/company.js
+++ spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/company.js
@@ -1,1 +1,1 @@
-var app = angular.module('company',[]);
+var app = angular.module('company', []);
--- spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/companyService.js
+++ spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/companyService.js
@@ -3,7 +3,14 @@ app.service('companyService', function($http) {
 	this.getData = function() {
 		return $http({
 			method : 'GET',
-			url : 'http://localhost:8080/companies'
+			url : 'http://localhost:8080/companies',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Credentials': 'true',
+				'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+				'Access-Control-Max-Age': '3600',
+				'Access-Control-Allow-Headers': 'Content-Type, Accept, X-Requested-With, remember-me'
+			}
 		});
 	}
 });
