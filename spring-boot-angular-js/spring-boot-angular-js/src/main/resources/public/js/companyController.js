8f5bb79ed6a7cd69edd7ed0c4c4f0aaeb794ddeb
Saravana <saravanahacker@gmail.com>
2016-02-16 00:24:16 +0530

    UI enhancement

--- spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/companyController.js
+++ spring-boot-angular-js/spring-boot-angular-js/src/main/resources/public/js/companyController.js
@@ -1,33 +1,41 @@
 app.controller('companyController', [ '$scope', 'companyService',
 		function($scope, companyService) {
-			
-			$scope.allCompanies = null;
-			
-			companyService.getAllCompanies().then(function(dataResponse) {
-				$scope.allCompanies = dataResponse.data;
-			});				
-			
-			$scope.newCompany = null;
-			
-			$scope.save = function(newCompany){				
-				console.log('Controller JSON '+JSON.stringify(newCompany));
-				companyService.addCompany(newCompany).then(function(dataResponse) {
-					$scope.newCompany = dataResponse.data;
+
+			$scope.allCompanies = [];
+
+			$scope.refresh = function() {
+				companyService.getAllCompanies().then(function(dataResponse) {
+					$scope.allCompanies = dataResponse.data;
 				});
 			};
-			
-			$scope.updatedCompany = null;
-			
-			companyService.updateCompany($scope.updatedCompany).then(function(dataResponse) {
-				$scope.updatedCompany = dataResponse.data;
-			});	
-			
-			$scope.company = null;
-			
-			$scope.getCompany = function (companyID){
-				companyService.getCompany(companyID).then(function(dataResponse) {
-					$scope.company = dataResponse.data;					
+
+			$scope.company = {};
+
+			$scope.reset = function() {
+				console.log('Reset In');
+				$scope.companyForm.$setUntouched();
+				$scope.companyForm.$setPristine();
+				$scope.comId = '';
+				console.log('Reset Out');
+			};
+
+			$scope.save = function(company) {
+				console.log('Controller JSON ' + JSON.stringify(company));
+				companyService.addCompany(company).then(function(dataResponse) {
+					$scope.company = dataResponse.data;
+					console.log('Company added.')
+					$scope.company = {};
+					$scope.companyForm.$setUntouched();
+					$scope.companyForm.$setPristine();
 				});
-			};	
-			
+
+			};
+
+			$scope.getCompany = function(comId) {
+				companyService.getCompany(comId).then(function(dataResponse) {
+					$scope.company = dataResponse.data;
+					$scope.comId = '';
+				});
+			};
+
 		} ]);
